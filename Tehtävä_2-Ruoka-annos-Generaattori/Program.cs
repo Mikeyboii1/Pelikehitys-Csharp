298eb714d2059a7b640c02198e316d40e659db05
Mikko Kirjalainen <s2401170@edu.bc.fi>
2026-03-09 09:50:54 +0200

    Tehtävä 2 - Ruoka-annos Generaattori
    Tehtävä 2 luotu

---
+++ Tehtävä_2-Ruoka-annos-Generaattori/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tehtävä_2_Ruoka_annos_Generaattori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
