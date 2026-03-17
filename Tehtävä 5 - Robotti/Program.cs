737ae3424381b0c35a5ae417341912510652802e
Mikko Kirjalainen <s2401170@edu.bc.fi>
2026-03-17 16:58:28 +0200

    tehtävä 5 luotu

---
+++ Tehtävä 5 - Robotti/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tehtävä_5___Robotti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
