a9a590bdbd224a1aecdabe4d5488a6abc9b4e927
Mikko Kirjalainen <s2401170@edu.bc.fi>
2026-03-17 14:46:23 +0200

    Tehtävä 3 luotu

---
+++ Tehtävä 3 - Nuolia kaupan/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tehtävä_3___Nuolia_kaupan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
