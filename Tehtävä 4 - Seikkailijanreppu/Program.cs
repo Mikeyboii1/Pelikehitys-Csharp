01c09fc60f4acb778ef380f15576ded09d7f8aaa
Mikko Kirjalainen <s2401170@edu.bc.fi>
2026-03-17 15:38:22 +0200

    Tehtävä 4 luotu

---
+++ Tehtävä 4 - Seikkailijanreppu/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tehtävä_4___Seikkailijanreppu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
